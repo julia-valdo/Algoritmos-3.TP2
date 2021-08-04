3b7125e6650ef35f2c4d25bce71abbf651571649
lvaldo <71952799+lvaldo@users.noreply.github.com>
2021-08-04 19:12:00 -0300

    Agrego musica de avance

--- src/main/java/edu/fiuba/algo3/musica/ControladorMusica.java
+++ src/main/java/edu/fiuba/algo3/musica/ControladorMusica.java
@@ -11,5 +11,12 @@ public class ControladorMusica{
         mediaPlayer.play();
         mediaPlayer.setVolume(7);
     }
+
+    public void startCambioEscena(){
+        AudioClip mediaPlayer = new AudioClip("file:campana_aoe.mp3");
+        //mediaPlayer.setCycleCount(AudioClip.INDEFINITE);
+        mediaPlayer.play();
+        mediaPlayer.setVolume(7);
+    }
 }
 
